ea3a41bab7fe34c9022bdfd9f8c774e21c26c097
Piumi Perera <piumidp96@gmail.com>
2021-04-16 10:10:43 +0530

    Remove unwanted files

--- api/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('./controller');
-
-router.get('/api/', controller.getCode);
-
-module.exports = router;
